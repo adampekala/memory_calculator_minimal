ee024bb39c3000537a393ba5442aa5b8109f2922
adampekala <adaspekala@wp.pl>
2025-10-06 18:04:03 +0200

    developing of statistic table - add option togglers

--- dist/createStatisticsTableHelper.js
+++ dist/createStatisticsTableHelper.js
@@ -1,13 +1,20 @@
 export const createStatisticTable = (body, storage, refreshStorageFn) => {
     let statisticContainer = document.createElement("div");
     statisticContainer.classList.add("statistics_container");
+    let header = document.createElement("div");
+    header.innerHTML = `<h1 class="title"><span class="title-top">Memory</span><span class="title-bottom">calculator</span></h1>`;
     let statisticTableContainer = document.createElement("div");
+    statisticTableContainer.classList.add("table_container");
+    statisticTableContainer.innerHTML = `<div class="table_modeContainer"><button class="table_button table_button-mode" id="tableMode_toggler">+</button>
+  
+  <button class="table_button table_button-mode" id="tableMode_toggler">&#x2217;</button>
+  <button class="table_button table_button-mode" id="tableMode_toggler">-</button>
+  </div><table><thead><tr><th>First number</th><th>Secound number</th><th>Delete</th></tr></thead><tbody></tbody></table>`;
     let statisticBackBtnContainer = document.createElement("div");
     statisticBackBtnContainer.classList.add("statistics_backButtonContainer");
-    statisticTableContainer.classList.add("table_container");
-    statisticTableContainer.innerHTML = `<table><thead><tr><th>First number</th><th>Secound number</th><th>Delete</th></tr></thead><tbody></tbody></table>`;
     statisticBackBtnContainer.innerHTML = `<button class="options_button options_button-back" id="backToOptions">Back</button><button class="options_button" id="deleteStorage">Delete all</button>`;
     body.appendChild(statisticContainer);
+    statisticContainer.appendChild(header);
     statisticContainer.appendChild(statisticTableContainer);
     statisticContainer.appendChild(statisticBackBtnContainer);
     console.log(statisticTableContainer.children[0]);
@@ -17,7 +24,7 @@ export const createStatisticTable = (body, storage, refreshStorageFn) => {
     storage.mistakesMultiply.forEach((values) => {
         let newElement = document.createElement("tr");
         newElement.innerHTML = `<tr><td>${values.split("--")[0]}</td><td>${values.split("--")[1]}</td><td><i class="fa fa-trash-o"></i></td></tr> `;
-        statisticTableContainer.children[0].children[1].appendChild(newElement);
+        statisticTableContainer.children[1].children[1].appendChild(newElement);
     });
     backToOptionsBtn.addEventListener("click", () => {
         statisticContainer.remove();
@@ -26,7 +33,7 @@ export const createStatisticTable = (body, storage, refreshStorageFn) => {
         localStorage.clear();
         refreshStorageFn();
         console.log("XXXX");
-        statisticTableContainer.children[0].children[1].innerHTML = "";
+        statisticTableContainer.children[1].children[1].innerHTML = "";
         console.log(statisticTableContainer.children[0].children[1]);
         // storage.mistakesMultiply.forEach((values) => {
         //   let newElement = document.createElement("tr");
--- src_ts/createStatisticsTableHelper.ts
+++ src_ts/createStatisticsTableHelper.ts
@@ -3,16 +3,23 @@ import { getAndConvertStorage } from "./helpers";
 export const createStatisticTable = (body, storage, refreshStorageFn) => {
   let statisticContainer = document.createElement("div");
   statisticContainer.classList.add("statistics_container");
+  let header = document.createElement("div");
+  header.innerHTML = `<h1 class="title"><span class="title-top">Memory</span><span class="title-bottom">calculator</span></h1>`;
+
   let statisticTableContainer = document.createElement("div");
+  statisticTableContainer.classList.add("table_container");
+  statisticTableContainer.innerHTML = `<div class="table_modeContainer"><button class="table_button table_button-mode" id="tableMode_toggler">+</button>
+  
+  <button class="table_button table_button-mode" id="tableMode_toggler">&#x2217;</button>
+  <button class="table_button table_button-mode" id="tableMode_toggler">-</button>
+  </div><table><thead><tr><th>First number</th><th>Secound number</th><th>Delete</th></tr></thead><tbody></tbody></table>`;
+
   let statisticBackBtnContainer = document.createElement("div");
   statisticBackBtnContainer.classList.add("statistics_backButtonContainer");
-
-  statisticTableContainer.classList.add("table_container");
-  statisticTableContainer.innerHTML = `<table><thead><tr><th>First number</th><th>Secound number</th><th>Delete</th></tr></thead><tbody></tbody></table>`;
-
   statisticBackBtnContainer.innerHTML = `<button class="options_button options_button-back" id="backToOptions">Back</button><button class="options_button" id="deleteStorage">Delete all</button>`;
 
   body.appendChild(statisticContainer);
+  statisticContainer.appendChild(header);
   statisticContainer.appendChild(statisticTableContainer);
   statisticContainer.appendChild(statisticBackBtnContainer);
 
@@ -27,7 +34,7 @@ export const createStatisticTable = (body, storage, refreshStorageFn) => {
     newElement.innerHTML = `<tr><td>${values.split("--")[0]}</td><td>${
       values.split("--")[1]
     }</td><td><i class="fa fa-trash-o"></i></td></tr> `;
-    statisticTableContainer.children[0].children[1].appendChild(newElement);
+    statisticTableContainer.children[1].children[1].appendChild(newElement);
   });
 
   backToOptionsBtn.addEventListener("click", () => {
@@ -38,7 +45,7 @@ export const createStatisticTable = (body, storage, refreshStorageFn) => {
     refreshStorageFn();
     console.log("XXXX");
 
-    statisticTableContainer.children[0].children[1].innerHTML = "";
+    statisticTableContainer.children[1].children[1].innerHTML = "";
 
     console.log(statisticTableContainer.children[0].children[1]);
 
